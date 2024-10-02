d2cfb5e931cd63cce1aae7f9bf6108f296ff13e6
Ruturaj mudholkar <ruturaj.m4@gmail.com>
2024-10-02 23:55:59 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import LoginCheck from './components/login';
+
+function App() {
+  return (
+    <div className="App">
+     <LoginCheck/>
+    </div>
+  );
+}
+
+export default App;
